c5cdbb70a5bb9cfaadaab55513e3cd04799404d3
suciaristarahmawati03-hue <suciaristarahmawati03@gmail.com>
2026-09-05 19:50:37 +0700

    Add files via upload

---
+++ identitas_diri.py.py
@@ -0,0 +1,11 @@
+print("\t\tidentitas diri");
+print("nama                :suci arista") #nama lengkap
+print("nim                 :", 2605060055) #nim
+print("tempat tanggal lahir: magelang", 16, "september",2007)# ttl
+print("jenis kelamin       : perempuan") #jk
+print("hobi                : berenang") #hobi
+
+
+print("\t\tidentitas diri")
+print("\n halloo teman")
+print("\b nama aku suci arista")
